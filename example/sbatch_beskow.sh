5c1a57ba37a95ab57e61f29801cd4f4b1f955c03
Xin Li <lixin.reco@gmail.com>
2019-08-08 14:53:49 +0200

    Updated tex files for summer school 2019

--- example/sbatch_beskow.sh
+++ example/sbatch_beskow.sh
@@ -2,11 +2,13 @@
 #SBATCH -J myjob
 # Defined the time allocation you use
 #SBATCH -A <time allocation>
-# 10 minute  wall-clock time will be given to this job
-#SBATCH -t 10:00
+# 10 minute wall-clock time will be given to this job
+#SBATCH -t 00:10:00
 # Number of nodes
 #SBATCH --nodes=2
+# Number of MPI processes per node
+#SBATCH --ntasks-per-nodes=32
 # load GNU compiler and mpi
 module swap PrgEnv-cray PrgEnv-gnu
 # Run program 
-aprun -n 32 -N 16 ./hello_world_mpi
+srun ./hello_world_mpi
